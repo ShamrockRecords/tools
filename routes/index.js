783377f098bb51ebadb37b8224e1350b1fc42638
青木秀仁 <shamrock1976@me.com>
2021-12-24 22:22:33 +0900

    OGPを修正

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router() ;
 const wrap = fn => (...args) => fn(...args).catch(args[2]) ;
 
 router.get('/', wrap(async function(req, res, next) {
-    res.render('index', {});		 
+    res.render('index', {rootURL: process.env.ROOT_URL});		 
 })) ;
 
 module.exports = router;
--- routes/srtToCsv/index.js
+++ routes/srtToCsv/index.js
@@ -4,7 +4,7 @@ var router = express.Router() ;
 const wrap = fn => (...args) => fn(...args).catch(args[2]) ;
 
 router.get('/', wrap(async function(req, res, next) {
-    res.render('tools/srtToCsv/index', {rootURL: process.env.ROOT_URL});		 
+    res.render('tools/srtToCsv/index', {rootURL: process.env.ROOT_URL + "/srt2csv"});		 
 })) ;
 
 module.exports = router;
