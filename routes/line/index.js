d91da0a244ed3c680e6e889555846da34abc8f55
青木秀仁 <shamrock1976@me.com>
2023-08-27 21:40:36 +0900

    動画検索へのURLを追加

--- routes/line/index.js
+++ routes/line/index.js
@@ -26,6 +26,16 @@ router.post('/webhook', wrap(async function(req, res, next) {
                     type: 'text',
                     text: replyMessage,
                 }) ;
+
+                let keywords = await geKeywords(replyMessage) ;
+                let array = keywords.split(",") ;
+
+                let URL = `動画から検索\nhttps://capsearch.udtalk.jp/search/udtalk?pid=&q=${encodeURI(array.join("|"))}` ;
+                
+                bot.replyMessage (event.replyToken, {
+                    type: 'text',
+                    text: URL,
+                }) ;
             }
         }) ;
     } catch (e) {
@@ -35,12 +45,59 @@ router.post('/webhook', wrap(async function(req, res, next) {
     res.status(200).send("OK") ;
 })) ;
 
+router.post('/debug', wrap(async function(req, res, next) {
+    let replyMessage = await getReplyMessage(req.body.prompt) ;
+
+    let keywords = await geKeywords(replyMessage) ;
+    let array = keywords.split(",") ;
+
+    let URL = `動画から検索\nhttps://capsearch.udtalk.jp/search/udtalk?pid=&q=${encodeURI(array.join("|"))}` ;
+                
+    res.send(JSON.stringify({replyMessage: replyMessage, URL: URL})) ;
+})) ;
+
 async function getReplyMessage(prompt) {
     let authorization = process.env.OPEN_AI_KEY ;
 
     let messages = [
         {"role": "system", "content": process.env.OPEN_AI_SYSTEM_PROMPT},
         {"role": "user", "content": prompt},
+    ] ;
+
+    let body = {
+        "model": process.env.OPEN_AI_MODEL,
+        "messages": messages,
+        "temperature": 0.0
+    } ;
+
+    const param = {
+        method: "POST",
+        headers: {"Authorization": "Bearer " + authorization, "Content-Type": "application/json"},
+        body: JSON.stringify(body)
+    }
+    
+    let data = await fetch("https://api.openai.com/v1/chat/completions", param).then(response => response.json()) ;
+    let content = "" ;
+
+    try {
+        if (data.error != undefined) {
+            content = data.error.message ;
+        } else if (data.choices[0].message != undefined) {
+            content = data.choices[0].message.content ;
+        }
+    } catch (e) {
+        content = e.message ;
+    }
+
+    return content ;
+}
+
+async function geKeywords(prompt) {
+    let authorization = process.env.OPEN_AI_KEY ;
+
+    let messages = [
+        {"role": "system", "content": "オンラインマニュアルを検索するために使えそうなキーワードを与えられた文章の中から3つくらい作ります。結果はカンマ区切りで返します。"},
+        {"role": "user", "content": "---" + prompt},
     ] ;
 
     let body = {
