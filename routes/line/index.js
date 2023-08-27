7634ef2d4aef7fa70e66d051fd74614dbd1a5494
青木秀仁 <shamrock1976@me.com>
2023-08-27 21:48:20 +0900

    文言を修正

--- routes/line/index.js
+++ routes/line/index.js
@@ -25,7 +25,7 @@ router.post('/webhook', wrap(async function(req, res, next) {
                 let keywords = await geKeywords(replyMessage) ;
                 let array = keywords.split(",") ;
 
-                let URL = `『動画を検索】\nhttps://capsearch.udtalk.jp/search/udtalk?pid=&q=${encodeURI(array.join("|"))}` ;
+                let URL = `【キーワードで動画を検索】\nhttps://capsearch.udtalk.jp/search/udtalk?pid=&q=${encodeURI(array.join("|"))}` ;
                 
                 bot.replyMessage (event.replyToken, {
                     type: 'text',
