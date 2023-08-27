86be4ec42aab9ed71f58d1fff46d3ea4b223e0f0
青木秀仁 <shamrock1976@me.com>
2023-08-27 19:32:32 +0900

    不具合を修正

--- routes/line/index.js
+++ routes/line/index.js
@@ -7,13 +7,13 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]) ;
 router.post('/webhook', wrap(async function(req, res, next) {
     
     const config = { 
-        channelAccessloken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+        channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
         channelSecret: process.env.LINE_CHANNEL_SECRET,
     } ;
-    
+
     const bot = new line.Client (config);
 
-    const events = rea-body .events ;
+    const events = res.body.events ;
 
     events.forEach (async (event) => {
 
